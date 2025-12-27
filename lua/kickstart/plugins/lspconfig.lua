1f74dda0ba03e1d185a9fd20231d10a76ccb0c7c
Bailey Vogt <s3906263@student.rmit.edu.au>
2025-12-28 04:09:16 +1100

    update gitsigns and lsp visual configs

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -26,6 +26,21 @@ return {
       'saghen/blink.cmp',
     },
     config = function()
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "",
+          }
+        },
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true
+      })
+
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       local cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
       if cmp_ok then
