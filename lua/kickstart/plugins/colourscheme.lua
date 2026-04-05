c1acdc715b754ea238bdbfa81c8751c615ad236c
Bailey Vogt <s3906263@student.rmit.edu.au>
2026-04-06 05:11:08 +1000

    update colourscheme

--- lua/kickstart/plugins/colourscheme.lua
+++ lua/kickstart/plugins/colourscheme.lua
@@ -4,7 +4,7 @@ return {
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
+    'EdenEast/nightfox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
@@ -12,7 +12,7 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'carbonfox'
     end,
   },
 }
